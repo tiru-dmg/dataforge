6053cdf3553ed8edf52eab727f1f65d7aa3f9351
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2024-10-26 20:04:13 +0530

    Update Kmeans

--- Kmeans.java
+++ Kmeans.java
@@ -1,47 +1,50 @@
-import java.io.*;
 import java.lang.*;
 
-class Kmean {
+class Main{
     public static void main(String args[]) {
         int N = 9;
         int arr[] = {2, 4, 10, 12, 3, 20, 30, 11, 25}; // initial data
         int i, m1, m2, a, b, n = 0;
         boolean flag = true;
-        float sum1 = 0, sum2 = 0;
-        a = arr[0]; 
+        a = arr[0];
         b = arr[1];
-        m1 = a; 
+        m1 = a;
         m2 = b;
-        int cluster1[] = new int[9];
-        int cluster2[] = new int[9];
+        int cluster1[] = new int[N];
+        int cluster2[] = new int[N];
 
-        for (i = 0; i < 9; i++)
+        System.out.println("Initial data:");
+        for (i = 0; i < N; i++)
             System.out.print(arr[i] + "\t");
         System.out.println();
 
         do {
             n++;
             int k = 0, j = 0;
-            sum1 = 0; // Reset sum1 and sum2 for each iteration
-            sum2 = 0;
+            float sum1 = 0, sum2 = 0; // Reset sum1 and sum2 for each iteration
 
-            for (i = 0; i < 9; i++) {
+            for (i = 0; i < N; i++) {
                 if (Math.abs(arr[i] - m1) <= Math.abs(arr[i] - m2)) {
                     cluster1[k] = arr[i];
+                    sum1 += arr[i];
                     k++;
                 } else {
                     cluster2[j] = arr[i];
+                    sum2 += arr[i];
                     j++;
                 }
             }
 
-            m1 = Math.round(sum1 / k);
-            m2 = Math.round(sum2 / j);
+            int newM1 = k == 0 ? m1 : Math.round(sum1 / k);
+            int newM2 = j == 0 ? m2 : Math.round(sum2 / j);
 
-            if (m1 == a && m2 == b)
+            if (newM1 == m1 && newM2 == m2)
                 flag = false;
             else
                 flag = true;
+
+            m1 = newM1;
+            m2 = newM2;
 
             System.out.println("After iteration " + n + ", cluster 1:");
             for (i = 0; i < k; i++)
@@ -53,19 +56,16 @@ class Kmean {
                 System.out.print(cluster2[i] + "\t");
             System.out.println();
 
-            a = m1;
-            b = m2;
-
         } while (flag);
 
         System.out.println("Final cluster 1:");
-        for (i = 0; i < N; i++)
-            if (i < k) System.out.print(cluster1[i] + "\t");
+        for (i = 0; i < N && cluster1[i] != 0; i++)
+            System.out.print(cluster1[i] + "\t");
         System.out.println();
 
         System.out.println("Final cluster 2:");
-        for (i = 0; i < N; i++)
-            if (i < j) System.out.print(cluster2[i] + "\t");
+        for (i = 0; i < N && cluster2[i] != 0; i++)
+            System.out.print(cluster2[i] + "\t");
         System.out.println();
     }
 }
