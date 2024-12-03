90a770e614c7905453ddc2f5af9536004051dff2
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2024-12-03 18:56:34 +0530

    Update Bgraph.py

--- Bgraph.py
+++ Bgraph.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 
-data = {'Category': ['A', 'B', 'C', 'D'], 'Values': [20, 35, 30, 25]}
-df = pd.DataFrame(data)
-plt.bar(df['Category'], df['Values'])
+plt.bar(['A', 'B', 'C', 'D'], [20, 35, 30, 25])
 plt.xlabel('Category')
 plt.ylabel('Values')
 plt.title('Bar Graph Example')
