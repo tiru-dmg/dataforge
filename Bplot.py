1a5be056b344e0f7c29abe25bfc72764c3750173
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2024-12-03 18:57:45 +0530

    Update Bplot.py

--- Bplot.py
+++ Bplot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 np.random.seed(10)
 data = np.random.normal(100, 20, 200)
-fig = plt.figure(figsize=(10, 7))
+
 plt.boxplot(data)
 plt.title('Boxplot of Random Data')
 plt.ylabel('Values')
